0abed2759011593ed46e22d07142ef3945cf4312
Thisselol007 <suetensmathisse@gmail.com>
2023-10-18 11:40:18 +0200

    added logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,67 +2,67 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <header class="bg-white">
+    <header className="bg-white">
   <div
-    class="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8"
+    className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8"
   >
-    <a class="block text-teal-600" href="/">
-      <span class="sr-only">Home</span>
-      
+    <a className="block text-teal-600" href="/">
+      <span className="sr-only">Home</span>
+      <img src="/Slagerij-logo.png" alt="" />
     </a>
 
-    <div class="flex flex-1 items-center justify-end md:justify-between">
-      <nav aria-label="Global" class="hidden md:block">
-        <ul class="flex items-center gap-6 text-sm">
+    <div className="flex flex-1 items-center justify-end md:justify-between">
+      <nav aria-label="Global" className="hidden md:block">
+        <ul className="flex items-center gap-6 text-sm">
           <li>
-            <a class="text-gray-500 transition hover:text-gray-500/75" href="/">
+            <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
               About
             </a>
           </li>
 
           <li>
-            <a class="text-gray-500 transition hover:text-gray-500/75" href="/">
+            <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
               Careers
             </a>
           </li>
 
           <li>
-            <a class="text-gray-500 transition hover:text-gray-500/75" href="/">
+            <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
               History
             </a>
           </li>
 
           <li>
-            <a class="text-gray-500 transition hover:text-gray-500/75" href="/">
+            <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
               Services
             </a>
           </li>
 
           <li>
-            <a class="text-gray-500 transition hover:text-gray-500/75" href="/">
+            <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
               Projects
             </a>
           </li>
 
           <li>
-            <a class="text-gray-500 transition hover:text-gray-500/75" href="/">
+            <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
               Blog
             </a>
           </li>
         </ul>
       </nav>
 
-      <div class="flex items-center gap-4">
-        <div class="sm:flex sm:gap-4">
+      <div className="flex items-center gap-4">
+        <div className="sm:flex sm:gap-4">
           <a
-            class="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
+            className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
             href="/"
           >
             Login
           </a>
 
           <a
-            class="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
+            className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
             href="/"
           >
             Register
@@ -70,20 +70,20 @@ export default function Navbar() {
         </div>
 
         <button
-          class="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
+          className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
         >
-          <span class="sr-only">Toggle menu</span>
+          <span className="sr-only">Toggle menu</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
+            className="h-5 w-5"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M4 6h16M4 12h16M4 18h16"
             />
           </svg>
