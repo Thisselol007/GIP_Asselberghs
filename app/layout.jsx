2fc4bf11385cbd134a166cc3775ca7db0fc5ebe9
Thisselol007 <suetensmathisse@gmail.com>
2023-10-21 16:56:21 +0200

    sign-in sign-out page, about-us page, home page layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,10 +11,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body classNameName={inter.classNameName}>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   )
 }
